cac5b015c3af53f94a3e4a6befe74b656b5487d2
“Nilde <nilde.benedetti@gmail.com>
2026-06-10 17:49:02 +0200

    add: scheletro di router in fikle products.js

---
+++ routers/products.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// importazione controllers
+
+//importazione middlewares
+
+// impostazione router
+
+const router = express.Router();
+
+// rotta index
+
+//rotta show
+
+export default router;
