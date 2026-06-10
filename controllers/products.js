df728971e0365b1553f7d8f2e117c410232258fe
BelliaNino <bellianino1996@gmail.com>
2026-06-10 23:49:02 +0200

    add show

--- controllers/products.js
+++ controllers/products.js
@@ -17,8 +17,35 @@ async function index(request, response) {
             results: null
         })
     }
-
-
 }
 
-export { index };
+//SHOW
+async function show(req, res) {
+  const realId = Number(req.params.id.trim());
+
+  if (isNaN(realId) || realId <= 0) {
+    res.status(400).json({ error: 'Id non valido', results: null });
+    return;
+  }
+
+  try {
+    const [rows] = await pool.query(
+      'SELECT * FROM `products` WHERE id = ?', [realId]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: `Prodotto ${realId} non trovato`, results: null });
+      return;
+    }
+
+    res.status(200).json({ error: null, results: rows[0] });
+  } catch (error) {
+    console.error("errore durante il recupero del prodotto", error.message);
+    res.status(500).json({
+      error: 'errore interno del server nel recupero del prodotto',
+      results: null
+    });
+  }
+}
+
+export { index, show };
