f9de95468e2a7e07ff78e2de2959f12895586acc
“Nilde <nilde.benedetti@gmail.com>
2026-06-12 13:37:50 +0200

    fix typo

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 function errorHandler(error, request, response, next) {
     console.log(error);
 
-    resnponse.status(500)
+    response.status(500)
     .json({
         error: error.message,
         results: null
