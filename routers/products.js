8db588a008b5a75343ec361369148e54c42843dd
“Nilde <nilde.benedetti@gmail.com>
2026-06-13 16:05:19 +0200

    aggiunta rotta custom per fetch avg rating per prodotto in products router e controller

--- routers/products.js
+++ routers/products.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import pool from '../utils/db.js';
-import { index, show, featured } from '../controllers/products.js';
+import { index, show, featured, showProductAvgStarRating } from '../controllers/products.js';
 import { validateProductBody } from '../middlewares/products.js';
 import { validateId } from '../middlewares/validateId.js';
 // importazione controllers
@@ -15,11 +15,14 @@ const router = express.Router();
 router.get('/', [ index]);
 
 // rottta product featured
-router.get('/featured', featured)
+router.get('/featured', [validateId, featured]);
 
 // rotta show
 
 router.get('/:id', [validateId, show]);
+
+// rotta custom per fetch star rating average
+router.get('/avgRating/:id', [validateId, showProductAvgStarRating]);
 
 
 
