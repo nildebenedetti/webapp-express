771c37320d65476882816e619fa05bfd5bfbecdf
“Nilde <nilde.benedetti@gmail.com>
2026-06-16 21:53:09 +0200

    fix rotta

--- routers/ai-agent.js
+++ routers/ai-agent.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { chat } from '../controllers/aiagentController.js';
 
 const router = express.Router();
 
 // rotta index con parametro ricerca
-router.get('/', [ index]);
+router.post('/chat', [ chat]);
 
 export default router;
